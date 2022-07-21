8722161f36f454fec8d349819d0fd879e052bcc7
MariaTu <wta49@sfu.ca>
2022-07-21 11:18:58 -0700

    first 10 lines of the files are extracted and displayed on the website

--- webserver_dataverse_files.py
+++ webserver_dataverse_files.py
@@ -1,13 +1,33 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import cgi
 
-# dataverse_files = { 1: 'B1E',2: 'B2E', 3: 'BME', 4: 'CDE', 5: 'CWE', 6:'DNE', 7:'DWE', 8: 'EBE', 9: 'EQE', 
-#                     10: 'FGE', 11: 'FRE', 12: 'FRG', 13: 'GRE', 14: 'HPE', 15 : 'HTE', 16: 'HTW' , 17: 'OFE', 
-#                     18: 'OUE', 19: 'TVE', 20: 'UTE', 21: 'WHE', 22: 'WHG', 23: 'WHW', 24: 'WOE'} 
+dataverse_files = { 'B1E': "C:\\Maria\\SFU\\SFU Thesis\\Figure 17 Research Paper\\dataverse_files\\excel_files\\B1E.CSV",
+'B2E': "C:\\Maria\\SFU\\SFU Thesis\\Figure 17 Research Paper\\dataverse_files\\excel_files\\B2E.CSV", 
+'BME': "C:\\Maria\\SFU\\SFU Thesis\\Figure 17 Research Paper\\dataverse_files\\excel_files\\BME.CSV", 
+'CDE': "C:\\Maria\\SFU\\SFU Thesis\\Figure 17 Research Paper\\dataverse_files\\excel_files\\CDE.CSV", 
+'CWE': "C:\\Maria\\SFU\\SFU Thesis\\Figure 17 Research Paper\\dataverse_files\\excel_files\\CWE.CSV", 
+'DNE': "C:\\Maria\\SFU\\SFU Thesis\\Figure 17 Research Paper\\dataverse_files\\excel_files\\DNE.CSV", 
+'DWE': "C:\\Maria\\SFU\\SFU Thesis\\Figure 17 Research Paper\\dataverse_files\\excel_files\\DWE.CSV", 
+'EBE': "C:\\Maria\\SFU\\SFU Thesis\\Figure 17 Research Paper\\dataverse_files\\excel_files\\EBE.CSV", 
+'EQE': "C:\\Maria\\SFU\\SFU Thesis\\Figure 17 Research Paper\\dataverse_files\\excel_files\\EQE.CSV", 
+'FGE': "C:\\Maria\\SFU\\SFU Thesis\\Figure 17 Research Paper\\dataverse_files\\excel_files\\FGE.CSV", 
+'FRE': "C:\\Maria\\SFU\\SFU Thesis\\Figure 17 Research Paper\\dataverse_files\\excel_files\\FRE.CSV", 
+'FRG': "C:\\Maria\\SFU\\SFU Thesis\\Figure 17 Research Paper\\dataverse_files\\excel_files\\FRG.CSV", 
+'GRE': "C:\\Maria\\SFU\\SFU Thesis\\Figure 17 Research Paper\\dataverse_files\\excel_files\\GRE.CSV", 
+'HPE': "C:\\Maria\\SFU\\SFU Thesis\\Figure 17 Research Paper\\dataverse_files\\excel_files\\HPE.CSV", 
+'HTE': "C:\\Maria\\SFU\\SFU Thesis\\Figure 17 Research Paper\\dataverse_files\\excel_files\\HTE.CSV", 
+'HTW': "C:\\Maria\\SFU\\SFU Thesis\\Figure 17 Research Paper\\dataverse_files\\excel_files\\HTW.CSV" , 
+'OFE': "C:\\Maria\\SFU\\SFU Thesis\\Figure 17 Research Paper\\dataverse_files\\excel_files\\OFE.CSV", 
+'OUE': "C:\\Maria\\SFU\\SFU Thesis\\Figure 17 Research Paper\\dataverse_files\\excel_files\\OUE.CSV", 
+'TVE': "C:\\Maria\\SFU\\SFU Thesis\\Figure 17 Research Paper\\dataverse_files\\excel_files\\TVE.CSV", 
+'UTE': "C:\\Maria\\SFU\\SFU Thesis\\Figure 17 Research Paper\\dataverse_files\\excel_files\\UTE.CSV", 
+'WHE': "C:\\Maria\\SFU\\SFU Thesis\\Figure 17 Research Paper\\dataverse_files\\excel_files\\WHE.CSV", 
+'WHG': "C:\\Maria\\SFU\\SFU Thesis\\Figure 17 Research Paper\\dataverse_files\\excel_files\\WHG.CSV", 
+'WHW': "C:\\Maria\\SFU\\SFU Thesis\\Figure 17 Research Paper\\dataverse_files\\excel_files\\WHW.CSV", 
+'WOE': "C:\\Maria\\SFU\\SFU Thesis\\Figure 17 Research Paper\\dataverse_files\\excel_files\\WOE.CSV"} 
 
-dataverse_files = { 1: 'B1E'} 
-data = []
 line_num = 10
+data = []
 
 class requestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -19,33 +39,41 @@ class requestHandler(BaseHTTPRequestHandler):
             output = ''
             output += '<html><body>'			
             output += '<h1>dataverse_files</h1>'
-            for (id,name) in dataverse_files.items():
-                output += '<h3><a href="/dataverse_files/extract"> %s </a></h3>' % dataverse_files[id]
-                # output += '<pre>  </pre>' #this should print out the list of dataverse_file in python
-            for index in data:
-                output += '<h3><a href="/dataverse_files"> %s </a></h3>' % data[index]
+            for (id,path_directory) in dataverse_files.items():
+                output += '<h3><a href="/dataverse_files/extract"> %s </a></h3>' % id
             output += '</body></html>'
             self.wfile.write(output.encode())
-
-
-    
-    def do_POST(self):
+        
         if self.path.endswith('/extract'):
-            ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
-            pdict['boundary'] = bytes(pdict['boundary'],"utf-8")
-            if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile,pdict)
-                max_id = max(list(data))     
-                with open("C:\Maria\SFU\SFU Thesis\Figure 17 Research Paper\dataverse_files\excel_files\B1E.csv") as myfile:
-                    head = [next(myfile) for x in range(line_num)]
-                    data[max_id+1] = head
-                    print(head)
-                myfile.close()
-
-            self.send_response(301)
+            self.send_response(200)
             self.send_header('content-type','text/html')
-            self.send_header('Location','/dataverse_files')
             self.end_headers()
+            output = ''
+            output += '<html><body>'
+            output += '<pre>'
+            for (id,path_directory) in dataverse_files.items():
+                with open(path_directory) as myfile:
+                    # head = [next(myfile) for x in range(line_num)]
+                    for x in range(line_num):
+                        output += next(myfile)                    
+            output += '</pre>'
+            output += '</body></html>'
+            # output = [information.encode('utf-8') for information in output]
+            self.wfile.write(output.encode())
+    
+    
+    # def do_POST(self):
+    #     if self.path.endswith('/extract'):
+    #         ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
+    #         pdict['boundary'] = bytes(pdict['boundary'],"utf-8")
+    #         fields = cgi.parse_multipart(self.rfile,pdict)
+    #         fields = head                
+    #         self.send_response(301)
+    #         self.send_header('content-type','text/html')
+    #         self.send_header('Location','/website')
+    #         self.end_headers()
+    #         self.wfile.write(output.encode())
+       
                      
         
 def main():
